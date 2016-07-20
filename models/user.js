e365aee42827bb2a8311805a9b5b2546195118a7
Bryan Jennings <bryanjenningz@gmail.com>
2016-07-20 14:36:39 -0700

    make email field unique

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 
 var userSchema = new mongoose.Schema({
   name: {type: String, required: true},
-  email: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   speaks: {type: String, required: true},
   learning: {type: String, required: true},
